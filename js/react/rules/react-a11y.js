bc7c93e10298cbd55b4e8421e0202dd8a9895ed7
Zac Rosenbauer <zacrosenbauer@gmail.com>
2016-12-19 09:12:35 -0600

    update reacta11y

--- js/react/rules/react-a11y.js
+++ js/react/rules/react-a11y.js
@@ -26,11 +26,15 @@ module.exports = {
 
     // ensure <hX> tags have content and are not aria-hidden
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/heading-has-content.md
-    'jsx-a11y/heading-has-content': [2, ['']],
+    'jsx-a11y/heading-has-content': [2, {
+      components: ['']
+    }],
 
     // disallow href "#"
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/href-no-hash.md
-    'jsx-a11y/href-no-hash': [2, ['a']],
+    'jsx-a11y/href-no-hash': [2, {
+      components: ['Link', 'Anchor', 'a']
+    }],
 
     // require HTML elements to have a "lang" prop
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/html-has-lang.md
@@ -46,7 +50,9 @@ module.exports = {
 
     // require that JSX labels use "htmlFor"
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
-    'jsx-a11y/label-has-for': [2, ['label']],
+    'jsx-a11y/label-has-for': [2, {
+      components: ['Label']
+    }],
 
     // require HTML element's lang prop to be valid
     // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/lang.md
